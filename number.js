cb44daa9c3769287182b89e9b09f6fc4c12c220a
jakariahossain24 <jakariahossain00011@gmail.com>
2025-07-27 08:11:43 +0600

    parseInt & parseFloat

--- number.js
+++ number.js
@@ -6,3 +6,10 @@ var applePrice=parseFloat('20.6');
 console.log(typeof orangePrice);
 console.log(typeof applePrice);
 console.log(applePrice + orangePrice);
+
+var first=0.1;
+
+var second=0.2;
+var sum=first+second;
+
+console.log(sum.toFixed(3));
