739c39f2163ef100318b1cb92312464a072b82bb
jakariahossain24 <jakariahossain00011@gmail.com>
2025-07-27 08:07:41 +0600

    parseInt

--- number.js
+++ number.js
@@ -1,6 +1,7 @@
 var orangePrice=20;
 var chocolate=9.5;
-var applePrice='20';
+// var applePrice=parseInt('20');
+var applePrice=parseFloat('20.6');
 
 console.log(typeof orangePrice);
 console.log(typeof applePrice);
